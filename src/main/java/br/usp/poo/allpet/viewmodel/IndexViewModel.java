abbccf10250fb1e79a818c89d09125bbf67016ff
gp2112 <guipaixao2000@gmail.com>
2021-07-25 23:17:07 -0300

    retorna username logado pra view

--- src/main/java/br/usp/poo/allpet/viewmodel/IndexViewModel.java
+++ src/main/java/br/usp/poo/allpet/viewmodel/IndexViewModel.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Map;
 
 import br.usp.poo.allpet.model.Anuncio;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 public class IndexViewModel extends ViewModel {
 
@@ -17,6 +20,11 @@ public class IndexViewModel extends ViewModel {
 	
 	@Override
 	public Map<String, Object> getParams() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (!(authentication instanceof AnonymousAuthenticationToken)) {
+			String currentUserName = authentication.getName();
+			params.put("username", currentUserName);
+		}
 		return params;
 	}
 
